916c4c251b10bf98e252982e3987c57c9c35c792
Brendan Coletta <bc.coletta@gmail.com>
2025-01-20 12:57:22 -0700

    Updated masthead

--- components/masthead.vue
+++ components/masthead.vue
@@ -7,9 +7,14 @@
   <h1 class="text-xl font-bold">Welcome to Brendan Rates Movies!</h1>
   <div>
     <!-- TODO - Better copy -->
-    Since 2019, I have kept track of movies that I've watched and rated them based on
-    how interesting the story was and how entertaining I found it.
+    Hi! I'm Brendan and since 2019 I have been rating movies based on how interesting I found the
+    story and how entertaining I thought the movie was as a whole.
   </div>
+
+  <hr class="my-2 border-slate-500 border-dashed" />
+
+  <div>Use the "Ratings" tab to view the list and chart containing all movies watched.</div>
+  <div>Use the "Stats" tab for breakdowns by the year and month that I watched each movie.</div>
 </div>
 </template>
 
